08d65b361a18df81001f76e095c9ba4f21d572ae
Enrique-mac <carvalhoenrique33@gmail.com>
2024-07-19 17:17:41 -0300

    added basic files for project

---
+++ Mini06AR/Mini06AR/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mini06AR
+//
+//  Created by Enrique Carvalho on 19/07/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ Mini06AR/Mini06AR/Model/PlanetModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlanetModel.swift
+//  Mini06AR
+//
+//  Created by Enrique Carvalho on 19/07/24.
+//
+
+import Foundation
