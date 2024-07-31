32a817467084044f76e611a16c9caaee0a2ada64
Gabriel Cardoso <74602964+gabriel144p@users.noreply.github.com>
2024-07-31 17:41:01 -0300

    Localizable

--- Mini06AR/Mini06AR/Model/PlanetData.swift
+++ Mini06AR/Mini06AR/Model/PlanetData.swift
@@ -14,111 +14,111 @@ import Foundation
 let planets = [
     
     // MARK: - Lua
-    Planet(name: "Lua",
+    Planet(name: "Moon",
            radius: 1737.1,
            distanceFromSun: 149.6,
            descriptions: [
-            "Descrição": "O único satélite natural da Terra, conhecido por sua influência nas marés.",
-            "Curiosidade 1": "A Lua é o quinto maior satélite natural do Sistema Solar.",
-            "Curiosidade 2": "A face visível da Lua é sempre a mesma devido à rotação sincronizada com a Terra."
+            "Descrição": "The only natural satellite of Earth, known for its influence on tides.",
+            "Curiosidade 1": "The Moon is the fifth largest natural satellite in the Solar System.",
+            "Curiosidade 2": "The visible face of the Moon is always the same due to its synchronized rotation with Earth."
            ],
            modelName: "moon.usdz"),
     // MARK: - Sol
-    Planet(name: "Sol",
+    Planet(name: "Sun",
            radius: 696340.0,
            distanceFromSun: 0.0,
            descriptions: [
-            "Descrição": "A estrela no centro do Sistema Solar, responsável por fornecer luz e calor à Terra.",
-            "Curiosidade 1": "O Sol é composto principalmente por hidrogênio e hélio.",
-            "Curiosidade 2": "A temperatura na superfície do Sol é de cerca de 15 milhões de graus Celsius."
+            "Descrição": "The star at the center of the Solar System, responsible for providing light and heat to Earth.",
+            "Curiosidade 1": "The Sun is primarily composed of hydrogen and helium.",
+            "Curiosidade 2": "The temperature at the surface of the Sun is about 15 million degrees Celsius."
            ],
            modelName: "sun.usdz"),
     
     // MARK: - Mercúrio
-    Planet(name: "Mercúrio",
+    Planet(name: "Mercury",
            radius: 2439.7,
            distanceFromSun: 57.9,
            descriptions: [
-            "Descrição": "O menor planeta do Sistema Solar e o mais próximo do Sol.",
-            "Curiosidade 1": "Mercúrio não tem atmosfera significativa para reter o calor.",
-            "Curiosidade 2": "A superfície de Mercúrio é coberta de crateras."
+            "Descrição": "The smallest planet in the Solar System and the closest to the Sun.",
+            "Curiosidade 1": "Mercury does not have a significant atmosphere to retain heat.",
+            "Curiosidade 2": "The surface of Mercury is covered with craters."
            ],
            modelName: "mercury.usdz"),
     
     // MARK: - Vênus
-    Planet(name: "Vênus",
+    Planet(name: "Venus",
            radius: 6051.8,
            distanceFromSun: 108.2,
            descriptions: [
-            "Descrição": "O segundo planeta do Sistema Solar e o mais quente.",
-            "Curiosidade 1": "A atmosfera de Vênus é composta principalmente de dióxido de carbono.",
-            "Curiosidade 2": "Vênus possui uma temperatura superficial média de 475 graus Celsius."
+            "Descrição": "The second planet in the Solar System and the hottest.",
+            "Curiosidade 1": "The atmosphere of Venus is primarily composed of carbon dioxide.",
+            "Curiosidade 2": "Venus has an average surface temperature of 475 degrees Celsius."
            ],
            modelName: "venus.usdz"),
     
     // MARK: - Terra
-    Planet(name: "Terra",
+    Planet(name: "Earth",
            radius: 6371.0,
            distanceFromSun: 149.6,
            descriptions: [
-            "Descrição": "Nosso lar, o terceiro planeta do Sistema Solar.",
-            "Curiosidade 1": "A Terra é o único planeta conhecido por abrigar vida.",
-            "Curiosidade 2": "A superfície da Terra é composta por 71% de água."
+            "Descrição": "Our home, the third planet in the Solar System.",
+            "Curiosidade 1": "Earth is the only known planet to harbor life.",
+            "Curiosidade 2": "The surface of Earth is composed of 71% water."
            ],
            modelName: "earth.usdz"),
     
     // MARK: - Marte
-    Planet(name: "Marte",
+    Planet(name: "Mars",
            radius: 3389.5,
            distanceFromSun: 227.9,
            descriptions: [
-            "Descrição": "O Planeta Vermelho, o quarto do Sistema Solar.",
-            "Curiosidade 1": "Marte possui a montanha mais alta do Sistema Solar, Olympus Mons.",
-            "Curiosidade 2": "A atmosfera de Marte é composta principalmente de dióxido de carbono."
+            "Descrição": "The Red Planet, the fourth in the Solar System.",
+            "Curiosidade 1": "Mars has the highest mountain in the Solar System, Olympus Mons.",
+            "Curiosidade 2": "The atmosphere of Mars is primarily composed of carbon dioxide."
            ],
            modelName: "mars.usdz"),
     
     // MARK: - Júpiter
-    Planet(name: "Júpiter",
+    Planet(name: "Jupiter",
            radius: 69911,
            distanceFromSun: 778.5,
            descriptions: [
-            "Descrição": "O maior planeta do Sistema Solar, o quinto a partir do Sol.",
-            "Curiosidade 1": "Júpiter tem uma mancha vermelha gigante, que é uma tempestade em curso.",
-            "Curiosidade 2": "Júpiter tem mais de 79 luas."
+            "Descrição": "The largest planet in the Solar System, the fifth from the Sun.",
+            "Curiosidade 1": "Jupiter has a giant red spot, which is an ongoing storm.",
+            "Curiosidade 2": "Jupiter has more than 79 moons."
            ],
            modelName: "jupiter.usdz"),
     
     // MARK: - Saturno
-    Planet(name: "Saturno",
+    Planet(name: "Saturn",
            radius: 58232,
            distanceFromSun: 1433.5,
            descriptions: [
-            "Descrição": "Conhecido pelos seus anéis impressionantes, é o sexto planeta do Sistema Solar.",
-            "Curiosidade 1": "Os anéis de Saturno são feitos principalmente de gelo e rocha.",
-            "Curiosidade 2": "Saturno tem 145 luas conhecidas."
+            "Descrição": "Known for its impressive rings, it is the sixth planet in the Solar System.",
+            "Curiosidade 1": "Saturn's rings are primarily made of ice and rock.",
+            "Curiosidade 2": "Saturn has 145 known moons."
            ],
            modelName: "saturn.usdz"),
     
     // MARK: - Urano
-    Planet(name: "Urano",
+    Planet(name: "Uranus",
            radius: 25362,
            distanceFromSun: 2872.5,
            descriptions: [
-            "Descrição": "O sétimo planeta do Sistema Solar, conhecido pela sua cor azulada.",
-            "Curiosidade 1": "Urano gira de lado, com seu eixo de rotação quase paralelo ao plano de sua órbita.",
-            "Curiosidade 2": "Urano tem 27 luas conhecidas."
+            "Descrição": "The seventh planet in the Solar System, known for its bluish color.",
+            "Curiosidade 1": "Uranus rotates on its side, with its axis of rotation almost parallel to the plane of its orbit.",
+            "Curiosidade 2": "Uranus has 27 known moons."
            ],
            modelName: "uranus.usdz"),
     
     // MARK: - Netuno
-    Planet(name: "Netuno",
+    Planet(name: "Neptune",
            radius: 24622,
            distanceFromSun: 4495.1,
            descriptions: [
-            "Descrição": "O oitavo planeta do Sistema Solar, conhecido por seus ventos fortes.",
-            "Curiosidade 1": "Netuno tem os ventos mais rápidos do Sistema Solar, atingindo até 2.100 km/h.",
-            "Curiosidade 2": "Netuno tem 14 luas conhecidas."
+            "Descrição": "The eighth planet in the Solar System, known for its strong winds.",
+            "Curiosidade 1": "Neptune has the fastest winds in the Solar System, reaching up to 2,100 km/h.",
+            "Curiosidade 2": "Neptune has 14 known moons."
            ],
            modelName: "neptune.usdz")
 ]
