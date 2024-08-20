bc5605d75fb06c68e521814a8db5a8f0a27a0942
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-08-20 16:30:27 -0300

    v3

--- Mini06AR/Mini06AR/Home/ViewController/HomeViewController.swift
+++ Mini06AR/Mini06AR/Home/ViewController/HomeViewController.swift
@@ -28,14 +28,12 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Cria uma nova HomeView com os limites da tela atual e uma lista de planetas.
         homeView = HomeView(frame: self.view.bounds, planets: planets)
         homeView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(planetTouch)))
         homeView.informationButton.addTarget(self, action: #selector(showBottomSheet), for: .touchUpInside)
         self.view.addSubview(homeView)
         
         
-        // Define a ação a ser realizada quando um planeta é selecionado.
         homeView.planetBoxView.showPlanetDetailView = { [weak self] planet in
             self?.coordinator?.showPlanetDetail(for: planet)
         }
@@ -60,14 +58,11 @@ class HomeViewController: UIViewController {
     @objc private func planetTouch(recognizer: UITapGestureRecognizer) {
         let loc = recognizer.location(in: homeView)
         
-        // Perform a hit test on the homeView.sceneView at the touch location
         let hitResults = homeView.sceneView.hitTest(loc, options: nil)
         
-        // Check if any of the hit results' nodes are children of the root node
         if let rootNode = homeView.sceneView.scene?.rootNode {
             for hitResult in hitResults {
                 if isDescendant(of: rootNode, node: hitResult.node) {
-                    // A child node of the root node was touched
                     focusCameraOnNode(hitResult.node)
                     
                     break
@@ -79,7 +74,6 @@ class HomeViewController: UIViewController {
     private func focusCameraOnNode(_ node: SCNNode) {
         guard let cameraNode = homeView.sceneView.pointOfView else { return }
 
-        // Calculate the bounding box center of the node
         let (min, max) = node.boundingBox
         let center = SCNVector3(
             (min.x + max.x) / 2,
@@ -87,17 +81,14 @@ class HomeViewController: UIViewController {
             (min.z + max.z) / 2
         )
 
-        // Convert the center to world coordinates
         let worldCenter = node.convertPosition(center, to: nil)
 
-        // Position the camera at a suitable distance from the node
         let distance: Float = 5
         let direction = SCNVector3(0, 0, distance)
         let cameraPosition = addVectors(left: worldCenter, right: direction)
 
-        // Set the camera's position and orientation
         SCNTransaction.begin()
-        SCNTransaction.animationDuration = 1 // Adjust the duration as needed
+        SCNTransaction.animationDuration = 1
         cameraNode.position = cameraPosition
         
         let lookAtNodeConstraint = SCNLookAtConstraint(target: node)
@@ -107,7 +98,6 @@ class HomeViewController: UIViewController {
         SCNTransaction.commit()
     }
 
-    // Helper operator to add two SCNVector3 vectors
     func addVectors(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
         return SCNVector3(left.x + right.x, left.y + right.y, left.z + right.z)
     }
